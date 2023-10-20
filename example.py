97249ca53978283d7bd6d1dc83593615783ef16a
muhammad <danfulani164@gmaail.com>
2023-10-20 12:09:24 +0100

    corrections

--- example.py
+++ example.py
@@ -3,3 +3,10 @@ print("test")
 #Assignment:
 #1. use print to display your name on the terminal 
 #2. use print to display your age on the terminal
+
+#corrections
+#1.
+print("muhammad")
+
+#2. 
+print(14)
--- example2.py
+++ example2.py
@@ -2,3 +2,6 @@ name="test"
 
 #excercise:
 #1. Write 5 examples of bad variable
+1name="jamal"
+#2. write 1 example of good variable
+name="jamal"
